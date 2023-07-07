2dde58c223dea7c0a0218b2f760841c27f19f022
Hamed Bahram <hamed.bahram@gmail.com>
2023-07-07 16:25:58 -0400

    Fix bug

--- app/components/products/ProductFilterSlider.tsx
+++ app/components/products/ProductFilterSlider.tsx
@@ -46,7 +46,7 @@ const ProductFilterSlider = ({
                 <button
                   type='button'
                   className='-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-stone-400'
-                  onClick={() => setMobileFiltersOpen(false)}
+                  onClick={() => onClose(false)}
                 >
                   <span className='sr-only'>Close menu</span>
                   <XMarkIcon className='h-6 w-6' aria-hidden='true' />
