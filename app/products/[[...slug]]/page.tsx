c32ee21de271c2794eeb49336edbc2d79c76ea03
Hamed Bahram <hamed.bahram@gmail.com>
2023-07-12 13:50:23 -0400

    Reorganize product dynamic routes, add cache

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,21 +1,35 @@
-import { getProducts, listCategories, sortMap } from '@/lib/swell/product'
+import {
+  getProducts,
+  listCategories,
+  sortMap,
+  getProductBySlugOrId
+} from '@/lib/swell/product'
+
 import Products from '@/components/products/Products'
+import Product from '@/app/components/products/Product'
 
 const Page = async ({
   params,
   searchParams
 }: {
-  params: { slug: string }
+  params: { slug?: string[] }
   searchParams: { sort?: string }
 }) => {
   const { slug } = params
+  const productSlug = slug?.length === 1 && slug[0]
+  const categorySlug = slug?.length === 2 && slug[1]
+
+  if (productSlug) {
+    const product = await getProductBySlugOrId(productSlug)
+    return <Product product={product} />
+  }
+
   const { sort } = searchParams
   const mappedSort = sort ? sortMap.get(sort) : ''
 
   const { results: products } = await getProducts({
-    page: 1,
     sort: mappedSort,
-    categories: [slug]
+    ...(categorySlug ? { categories: [categorySlug] } : {})
   })
 
   const { results: categories } = await listCategories()
--- lib/swell/product.ts
+++ lib/swell/product.ts
@@ -1,37 +1,42 @@
 import swell from '@/lib/swell/client'
 import { GetProductsInput } from '@/lib/types'
 import { PRODUCTS_PER_PAGE, CATEGORIES_PER_PAGE } from '@/lib/utils/constants'
+import { cache } from 'react'
 
-export const getProducts = async ({
-  page = 1,
-  sort = '',
-  search = '',
-  categories = [],
-  limit = PRODUCTS_PER_PAGE
-}: GetProductsInput) => {
-  const query = {
-    page,
-    limit,
-    sort,
-    search,
-    categories,
-    expand: ['variants', 'categories']
+export const getProducts = cache(
+  async ({
+    page = 1,
+    sort = '',
+    search = '',
+    categories = [],
+    limit = PRODUCTS_PER_PAGE
+  }: GetProductsInput) => {
+    const query = {
+      page,
+      limit,
+      sort,
+      search,
+      categories,
+      expand: ['variants', 'categories']
+    }
+
+    return await swell.products.list(query)
   }
+)
 
-  return await swell.products.list(query)
-}
-
-export const getProductBySlugOrId = async (slugOrId: string) => {
+export const getProductBySlugOrId = cache(async (slugOrId: string) => {
   return await swell.products.get(slugOrId)
-}
+})
 
-export const listCategories = async (limit = CATEGORIES_PER_PAGE, page = 1) => {
-  return await swell.categories.list({
-    limit,
-    page,
-    expand: ['products']
-  })
-}
+export const listCategories = cache(
+  async (limit = CATEGORIES_PER_PAGE, page = 1) => {
+    return await swell.categories.list({
+      limit,
+      page,
+      expand: ['products']
+    })
+  }
+)
 
 export const sortOptions = [
   { label: 'Latest', value: 'latest' },
